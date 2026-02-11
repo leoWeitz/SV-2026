7f1d80923e9b5d3f95f58790026d074104c09927
leoWeitz <lweitz@itba.edu.ar>
2026-02-12 00:34:46 +0100

    Image mural

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 function irPagina2() {
-    window.location.href = "pagina2.html";
+    window.location.href = "page2.html";
   }
+  
+  function irPagina3() {
+    window.location.href = "page3.html";
+  }
+  
+  /* Animación scroll */
+  const items = document.querySelectorAll(".mural-item");
+  
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  }, {
+    threshold: 0.2
+  });
+  
+  items.forEach(item => {
+    observer.observe(item);
+  });
   
